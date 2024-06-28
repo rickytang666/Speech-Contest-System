4895b38f3f8b9bd5c235daed5a3ac2ac447b75f0
Ruiqi Tang <tangricky001@gmail.com>
2024-06-28 09:33:34 -0400

    Added speaker class

---
+++ Speech Contest System/speaker.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+class Speaker
+{
+public:
+
+	string m_Name; // Name
+	double m_Score[2]; // Score -> At most 2 rounds
+};
